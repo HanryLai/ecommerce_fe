556260d961fca256e2fd4d05c39cb79b606337aa
Hieu Lai <ldmhieudev@gmail.com>
2024-11-03 16:04:32 +0700

    Feat: Add NavigationStackParamList and PropsNavigate types for navigation

---
+++ src/utils/types/Navigation.type.tsx
@@ -0,0 +1,10 @@
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+
+export type NavigationStackParamList = {
+    homepage: undefined;
+    login: undefined;
+    register: undefined;
+    Tab: undefined;
+    main: undefined;
+};
+export type PropsNavigate = NativeStackScreenProps<NavigationStackParamList>;
