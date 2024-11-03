518d2fbc1e75d86188bc59ee90ef58aa368b67e3
Hieu Lai <ldmhieudev@gmail.com>
2024-11-03 16:03:19 +0700

    Refactor: Update navigation structure to use RootNavigation component

--- App.tsx
+++ App.tsx
@@ -1,29 +1,17 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { Login } from "./src/component/login/login.auth";
-import { Register } from "./src/component/login/register.auth";
-import { HomePage } from "./src/component/home/homepage";
+import { RootNavigation } from "./src/utils/stacks/RootNavigation.stack";
 
 const Stack = createNativeStackNavigator();
 export default function App() {
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="login">
+            <Stack.Navigator initialRouteName="main">
                 <Stack.Screen
-                    name="login"
-                    component={Login}
-                    options={{ title: "Login" }}
-                ></Stack.Screen>
-                <Stack.Screen
-                    name="register"
-                    component={Register}
-                    options={{ title: "Register" }}
-                ></Stack.Screen>
-                <Stack.Screen
-                    name="homepage"
-                    component={HomePage}
-                    options={{ title: "Homepage" }}
-                ></Stack.Screen>
+                    name="main"
+                    component={RootNavigation}
+                    options={{ headerShown: false }}
+                />
             </Stack.Navigator>
         </NavigationContainer>
     );
