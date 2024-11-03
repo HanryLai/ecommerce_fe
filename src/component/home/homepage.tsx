82111f5b1dcc3e6e4c9b90108dc6bfc998b1ca47
Hieu Lai <ldmhieudev@gmail.com>
2024-11-03 16:03:47 +0700

    Feat: Refactor HomePage component to use useRoute for route parameters

--- src/component/home/homepage.tsx
+++ src/component/home/homepage.tsx
@@ -1,22 +1,19 @@
+import { RouteProp, useRoute } from "@react-navigation/native";
 import { Text, View } from "react-native";
-import { Props } from "../../common/types/props.type";
-import { RouteProp } from "@react-navigation/native";
+import { PropsData } from "../../common/types/props.type";
 
-type RootStackParamList = {
-    homepage: { id: string; username: string };
-};
-
-type PropsData = {
-    route: RouteProp<RootStackParamList, "homepage">;
-};
-
-export const HomePage = ({ route }: PropsData) => {
+export const HomePage = () => {
+    const route = useRoute<RouteProp<PropsData, "homepage">>();
     const { id, username } = route.params;
     return (
         <View>
             <Text>
-                Welcome <Text style={{ fontSize: 20, color: "#2241b9" }}>{username}</Text> to home
-                page
+                Welcome{"  "}
+                <Text style={{ fontSize: 20, color: "#2241b9", marginHorizontal: 20 }}>
+                    {username}
+                </Text>
+                {"  "}
+                to homepage
             </Text>
         </View>
     );
