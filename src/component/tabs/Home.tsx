053c7cfc02847620ffb6e6fcb30c7fac4856b329
tranvy57 <tranvy.art@gmail.com>
2024-11-04 17:22:47 +0700

    adad advertiment

--- src/component/tabs/Home.tsx
+++ src/component/tabs/Home.tsx
@@ -1,5 +1,14 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
-import { FlatList, Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import {
+	FlatList,
+	Image,
+	ScrollView,
+	StyleSheet,
+	Text,
+	TextInput,
+	TouchableOpacity,
+	View,
+} from 'react-native'
 import { TabStackParamList } from '../../utils/types'
 import AntDesign from '@expo/vector-icons/AntDesign'
 import { SafeAreaView } from 'react-native-safe-area-context'
@@ -7,13 +16,14 @@ import { SearchBar } from 'react-native-screens'
 import { ListRenderItem } from 'react-native'
 import { Category } from '../category'
 
-type Item = {
+// category
+type Category = {
 	id: string
 	name: string
 	image: string
 }
 
-const categories: Item[] = [
+const categories: Category[] = [
 	{ id: '1', name: 'Phone', image: 'phone' },
 	{ id: '2', name: 'Laptop', image: 'laptop' },
 	{ id: '3', name: 'Tablet', image: 'tablet' },
@@ -24,7 +34,7 @@ const categories: Item[] = [
 	{ id: '8', name: 'TV', image: 'tv' },
 ]
 
-const renderCategory: ListRenderItem<Item> = ({ item }) => (
+const renderCategory: ListRenderItem<Category> = ({ item }) => (
 	<TouchableOpacity style={styles.category}>
 		<View style={styles.circle}>
 			<Image source={require('../../../assets/categoryPhone.png')} />
@@ -38,38 +48,70 @@ type Props = NativeStackScreenProps<TabStackParamList, 'Home'>
 export const Home = ({ navigation, route }: Props) => {
 	return (
 		<SafeAreaView style={styles.container}>
-			{/* search bar */}
-			<View style={styles.SearchBar}>
-				<View style={[styles.search, { backgroundColor: '#F3F4F6', width: '90%' }]}>
-					<AntDesign name="search1" size={20} color="black" />
-					<TextInput placeholder="Search..." placeholderTextColor="gray" />
+			<ScrollView
+				contentContainerStyle={{ flexGrow: 1 }}
+				showsVerticalScrollIndicator={false}
+				showsHorizontalScrollIndicator={false}
+			>
+				{/* search bar */}
+				<View style={styles.SearchBar}>
+					<View style={[styles.search, { backgroundColor: '#F3F4F6', width: '90%' }]}>
+						<AntDesign name="search1" size={20} color="black" />
+						<TextInput placeholder="Search..." placeholderTextColor="gray" />
+					</View>
+
+					<TouchableOpacity
+						style={{
+							backgroundColor: '#F3F4F6',
+							width: '10%',
+							height: 46,
+							justifyContent: 'center',
+							alignItems: 'center',
+							borderRadius: 5,
+						}}
+					>
+						<AntDesign name="filter" size={24} color="black" />
+					</TouchableOpacity>
 				</View>
 
-				<TouchableOpacity
-					style={{
-						backgroundColor: '#F3F4F6',
-						width: '10%',
-						height: 46,
-						justifyContent: 'center',
-						alignItems: 'center',
-						borderRadius: 5,
-					}}
-				>
-					<AntDesign name="filter" size={24} color="black" />
-				</TouchableOpacity>
-			</View>
+				{/* category */}
+				<View style={styles.categories}>
+					<FlatList
+						data={categories}
+						renderItem={renderCategory}
+						keyExtractor={(item) => item.id}
+						horizontal={true}
+					/>
+				</View>
 
-			{/* category */}
-			<View style={styles.categories}>
-				<FlatList
-					data={categories}
-					renderItem={renderCategory}
-					keyExtractor={(item) => item.id}
-					horizontal={true}
-				/>
-			</View>
-
-			{/* advertisement */}
+				{/* advertisement */}
+				<View style={styles.advertisement}>
+					<View style={styles.advertisementTop}>
+						<View style={{ gap: 5, justifyContent: 'center' }}>
+							<Text style={{ color: '#00BDD6', fontSize: 24, fontWeight: 'bold' }}>Shoe</Text>
+							<Text style={{ color: '#929292', fontSize: 16 }}>50% off</Text>
+							<TouchableOpacity style={{ backgroundColor: 'black' }}>
+								<Text
+									style={{
+										color: 'white',
+										fontSize: 12,
+										fontWeight: 'bold',
+										paddingHorizontal: 10,
+										paddingVertical: 5,
+									}}
+								>
+									Buy now
+								</Text>
+							</TouchableOpacity>
+						</View>
+						<Image source={require('../../../assets/Snicker.png')} />
+					</View>
+					<View style={styles.advertisementBot}>
+						<Image source={require('../../../assets/bag.png')} style={{ width: '48%' }} />
+						<Image source={require('../../../assets/laptop.png')} style={{ width: '48%' }} />
+					</View>
+				</View>
+			</ScrollView>
 		</SafeAreaView>
 	)
 }
@@ -79,8 +121,8 @@ const styles = StyleSheet.create({
 		flex: 1,
 		justifyContent: 'flex-start',
 		alignItems: 'flex-start',
-		backgroundColor: 'pink',
-		padding: 20,
+		backgroundColor: 'white',
+		padding: 10,
 		gap: 20,
 	},
 	search: {
@@ -101,7 +143,7 @@ const styles = StyleSheet.create({
 	},
 	category: {
 		width: 85,
-		margin: 2,
+		margin: 4,
 	},
 	circle: {
 		backgroundColor: '#8353E2',
@@ -124,5 +166,14 @@ const styles = StyleSheet.create({
 		borderTopLeftRadius: 10,
 		paddingVertical: 15,
 		paddingHorizontal: 10,
+	},
+	advertisementBot: {
+		flexDirection: 'row',
+		justifyContent: 'space-around',
+		width: '100%',
+		borderBottomRightRadius: 10,
+		borderBottomLeftRadius: 10,
+		paddingVertical: 15,
+		gap: 10,
 	},
 })
