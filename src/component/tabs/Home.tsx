cc8f19f3a99a268a7562cc19b30099be3916d0e9
tranvy57 <tranvy.art@gmail.com>
2024-11-04 17:03:03 +0700

    add category list

--- src/component/tabs/Home.tsx
+++ src/component/tabs/Home.tsx
@@ -1,22 +1,51 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
-import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import { FlatList, Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import { TabStackParamList } from '../../utils/types'
 import AntDesign from '@expo/vector-icons/AntDesign'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { SearchBar } from 'react-native-screens'
+import { ListRenderItem } from 'react-native'
+import { Category } from '../category'
+
+type Item = {
+	id: string
+	name: string
+	image: string
+}
+
+const categories: Item[] = [
+	{ id: '1', name: 'Phone', image: 'phone' },
+	{ id: '2', name: 'Laptop', image: 'laptop' },
+	{ id: '3', name: 'Tablet', image: 'tablet' },
+	{ id: '4', name: 'Watch', image: 'watch' },
+	{ id: '5', name: 'Headphone', image: 'headphone' },
+	{ id: '6', name: 'Camera', image: 'camera' },
+	{ id: '7', name: 'Speaker', image: 'speaker' },
+	{ id: '8', name: 'TV', image: 'tv' },
+]
+
+const renderCategory: ListRenderItem<Item> = ({ item }) => (
+	<TouchableOpacity style={styles.category}>
+		<View style={styles.circle}>
+			<Image source={require('../../../assets/categoryPhone.png')} />
+		</View>
+		<Text style={{ textAlign: 'center' }}>{item.name}</Text>
+	</TouchableOpacity>
+)
 
 type Props = NativeStackScreenProps<TabStackParamList, 'Home'>
 
 export const Home = ({ navigation, route }: Props) => {
 	return (
 		<SafeAreaView style={styles.container}>
+			{/* search bar */}
 			<View style={styles.SearchBar}>
 				<View style={[styles.search, { backgroundColor: '#F3F4F6', width: '90%' }]}>
 					<AntDesign name="search1" size={20} color="black" />
 					<TextInput placeholder="Search..." placeholderTextColor="gray" />
 				</View>
 
-				<View
+				<TouchableOpacity
 					style={{
 						backgroundColor: '#F3F4F6',
 						width: '10%',
@@ -27,8 +56,20 @@ export const Home = ({ navigation, route }: Props) => {
 					}}
 				>
 					<AntDesign name="filter" size={24} color="black" />
-				</View>
+				</TouchableOpacity>
 			</View>
+
+			{/* category */}
+			<View style={styles.categories}>
+				<FlatList
+					data={categories}
+					renderItem={renderCategory}
+					keyExtractor={(item) => item.id}
+					horizontal={true}
+				/>
+			</View>
+
+			{/* advertisement */}
 		</SafeAreaView>
 	)
 }
@@ -36,10 +77,11 @@ export const Home = ({ navigation, route }: Props) => {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center',
+		justifyContent: 'flex-start',
+		alignItems: 'flex-start',
 		backgroundColor: 'pink',
 		padding: 20,
+		gap: 20,
 	},
 	search: {
 		flexDirection: 'row',
@@ -48,11 +90,39 @@ const styles = StyleSheet.create({
 		borderRadius: 5,
 	},
 	SearchBar: {
-		flex: 1,
 		justifyContent: 'center',
 		alignItems: 'center',
 		flexDirection: 'row',
 		gap: 10,
 		borderRadius: 5,
+	},
+	categories: {
+		flexDirection: 'row',
+	},
+	category: {
+		width: 85,
+		margin: 2,
+	},
+	circle: {
+		backgroundColor: '#8353E2',
+		width: 85,
+		height: 85,
+		borderRadius: 50,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	advertisement: {
+		borderRadius: 10,
+		width: '100%',
+	},
+	advertisementTop: {
+		flexDirection: 'row',
+		justifyContent: 'space-around',
+		width: '100%',
+		backgroundColor: '#F5F2FD',
+		borderTopRightRadius: 10,
+		borderTopLeftRadius: 10,
+		paddingVertical: 15,
+		paddingHorizontal: 10,
 	},
 })
