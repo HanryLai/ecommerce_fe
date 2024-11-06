03cfa966e71916f7521973da32f70e78ee212c07
Hieu Lai <ldmhieudev@gmail.com>
2024-11-06 11:22:09 +0700

    Feat: Enhance PropsTab type to utilize NativeStackScreenProps for improved type safety in TabStackParamList

--- src/utils/types/Tab.type.tsx
+++ src/utils/types/Tab.type.tsx
@@ -1,5 +1,12 @@
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+
 export type TabStackParamList = {
     Tab_1: undefined;
     Tab_2: undefined;
     TabRoot: undefined;
 };
+
+export type PropsTab<RouteName extends keyof TabStackParamList> = NativeStackScreenProps<
+    TabStackParamList,
+    RouteName
+>;
