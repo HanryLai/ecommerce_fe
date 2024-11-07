c7f2b97932e818419a9d457096beb3c4db67e775
Hieu Lai <ldmhieudev@gmail.com>
2024-11-07 18:31:22 +0700

    Feat: Remove Tab_2 component and replace with Favorite component in tab navigation

---
+++ src/component/tabs/favorite.tab.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+import { PropsTab } from "../../utils/types";
+export const Favorite = ({ navigation, route }: PropsTab<"Favorite">) => {
+    return (
+        <View>
+            <Text>Tab_2</Text>
+        </View>
+    );
+};
